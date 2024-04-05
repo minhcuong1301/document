9ea046980f2ce0fc6afc02c51aecc180f954ae21
tsong0ku <sonbappe89@gmail.com>
2024-04-05 10:54:22 +0700

    reformat modal interface( no responsive)

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -28,6 +28,7 @@ import {
   message,
   Popconfirm,
   Select,
+  Tag,
   Input,
 } from "antd";
 import EditUser from "./components/editUserModal";
@@ -238,14 +239,18 @@ const HomePage = () => {
               onConfirm={() => handleLock(r.id)}
             >
               {r.account_stutus === 1 && r.position_code !== "ADMIN" ? (
-                <Button className="btn-lock">
-                  <IoLockClosedOutline className="icon-fa icon-fa-lock" />
-                </Button>
+                <Tag color="#f41f1f" className="tag-btn">
+                  <Button type="text" className="btn-lock">
+                    <IoLockClosedOutline className="icon-fa icon-fa-lock" />
+                  </Button>
+                </Tag>
               ) : (
                 r.position_code !== "ADMIN" && (
-                  <Button danger className="btn-unlock">
-                    <GoUnlock className="icon-fa icon-fa-unlock" />
-                  </Button>
+                  <Tag color="#87d068" className="tag-btn">
+                    <Button type="text" className="btn-unlock">
+                      <GoUnlock className="icon-fa icon-fa-unlock" />
+                    </Button>
+                  </Tag>
                 )
               )}
             </Popconfirm>
